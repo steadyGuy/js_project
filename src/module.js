a8edca32f3400f891bc2b897b30b17ffaa94dbea
Yuriy <41287687+steadyGuy@users.noreply.github.com>
2020-06-17 18:56:19 +0300

    Finish project config

---
+++ src/module.js
@@ -0,0 +1,14 @@
+console.log('Module.js');
+
+/**
+ * Создает экземпляр Circle.
+ *
+ * @constructor
+ * @this  {Circle}
+ * @param {number} r - Радиус окружности.
+ */
+async function start() {
+  await Promise.resolve('working here');
+}
+
+start();
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const fileName = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ];
+
+  if (isDev) {
+    // webpack поддерживает такой формат (типо вместо объекта - строка)
+    loaders.push('eslint-loader');
+  }
+
+  return loaders;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: fileName('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@corenpm': path.resolve(__dirname, 'src/core'),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CopyPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist'),
+      }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: fileName('css'),
+    }),
+  ],
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: isDev,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: jsLoaders(),
+      },
+    ],
+  },
+};
